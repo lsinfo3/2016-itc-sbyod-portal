881c869e6cf94d3852d294a9ebcad350145b276b
Benedikt Pfaff <benedikt.pfaff@stud-mail.uni-wuerzburg.de>
2016-05-19 15:36:24 +0200

    bugfixes

--- collections.js
+++
@@ -1,4 +0,0 @@
-//all onos byod services available
-OnosServices = new Mongo.Collection("onosServices");
-//all services a user is allowed to use
-UserServices = new Mongo.Collection("userServices");
